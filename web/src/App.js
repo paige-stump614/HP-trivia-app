8d26a20d21191ce127ea2cdfb553d03a9b59b17a
paige-stump614 <paige.stump614@proton.me>
2023-10-04 19:57:39 -0400

    add front end react files to git

---
+++ web/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import api from './api/axiosConfig';
+import {useState, useEffect} from 'react';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
---
+++ web/src/api/axiosConfig.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+export default axios.create({
+ baseURL:'https://9c96-103-106-239-104.ap.ngrok.io',
+ //baseURL: 'http://192.168.1.92:8080/api/movies',
+ headers: {"ngrok-skip-browser-warning": "true"}
+});

0ed9aa2d6aafea71cced8c034bc8301ca4f1aa31
paige-stump614 <paige.stump614@proton.me>
2023-11-09 17:36:35 -0500

    add home page and submit question form - need to add validators

--- web/HPtrvia/src/app/submit-question/submit-question.component.ts
+++ web/HPtrvia/src/app/submit-question/submit-question.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-submit-question',
   templateUrl: './submit-question.component.html',
   styleUrls: ['./submit-question.component.css']
 })
-export class SubmitQuestionComponent {
+export class SubmitQuestionComponent implements OnInit {
+  questionForm = new FormGroup({
+    question: new FormControl(null),
+    answer: new FormControl(null),
+    type: new FormControl(null),
+    category: new FormControl(null),
+    book: new FormControl(null),
+    page: new FormControl(null),
+    topic: new FormControl(null)
+  });
 
-  onSubmit() {
+  ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+  console.log("form submitted");
+  console.log(form);
   }
 
 }
